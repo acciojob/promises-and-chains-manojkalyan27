87db76cb539676d6dcf700da2d7f95ae0d354f4d
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-11-21 15:32:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const btn = document.querySelector('#btn');
+const ageInput = document.querySelector('#age');
+const nam = document.querySelector('#name');
+
+btn.addEventListener("click", (event)=>{
+    event.preventDefault(); 
+    const age = parseInt(ageInput.value);
+    const name = nam.value;
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(age >18){
+                resolve();
+            } 
+            else{
+                reject()
+            }
+        },4000);
+    })
+    .then(()=>{
+            alert(`Welcome, ${name}. You can vote.`);
+        })
+        .catch(()=>{
+            alert(`Oh sorry ${name}. You aren't old enough.`);
+        });
